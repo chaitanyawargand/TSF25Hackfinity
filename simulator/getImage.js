af3f7a29f6747e8477ff8ea5096af062d43c2ba4
niharika896 <niharika9agarwal@gmail.com>
2025-10-11 19:37:58 +0530

    changes

--- simulator/getImage.js
+++ simulator/getImage.js
@@ -6,7 +6,7 @@ export async function sendImageToFastAPI(imagePath) {
   const form = new FormData();
   form.append("file", fs.createReadStream(imagePath)); // send as UploadFile
 
-  const response = await axios.post("http://http://127.0.0.1:8000/predict", form, {
+  const response = await axios.post("http://127.0.0.1:8000/predict", form, {
     headers: form.getHeaders(),
   });
 
